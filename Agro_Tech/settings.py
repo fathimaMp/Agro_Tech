60e0c21065b97a8a0999551046c2c97ef128a32c
fathima mp <fathimamp37@gmail.com>
2025-01-01 20:53:03 +0530

    committed

--- Agro_Tech/settings.py
+++ Agro_Tech/settings.py
@@ -77,14 +77,11 @@ WSGI_APPLICATION = 'Agro_Tech.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'agro_tech',
-         'USER': 'root',
-         'PASSWORD': '',
-         'HOST': 'localhost',
-         'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
